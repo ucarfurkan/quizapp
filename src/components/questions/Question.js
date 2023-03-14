dae51454fe8ccd9e457e6f94dda2d48daa6c1cdd
ucarfurkan <furkanucar011@gmail.com>
2023-03-14 10:46:28 +0300

    answering operations and score set

--- src/components/questions/Question.js
+++ src/components/questions/Question.js
@@ -2,9 +2,9 @@ import React from "react"
 import { nanoid } from 'nanoid'
 
 function Question(props) {
-    const { question } = props
+    const { question, setSelectedAnswers, selectedAnswers } = props
     const [options, setOptions] = React.useState([])
-
+    const [answer, setAnswer] = React.useState({ id: question.id, selectedAnswer: null })
     React.useEffect(() => {
         // shuffle options
         const optionList = [question.correctAnswer, ...question.incorrectAnswers]
@@ -13,19 +13,42 @@ function Question(props) {
             [optionList[i], optionList[j]] = [optionList[j], optionList[i]];
         }
         setOptions(optionList)
-    }, [question])
+    }, [])
+
+
+    React.useEffect(() => {
+        const index = selectedAnswers.findIndex(a => a.id === answer.id)
+        if (index === -1) {
+            setSelectedAnswers(prev => [...prev, answer])
+        } else {
+            const updatedAnswers = [...selectedAnswers]
+            updatedAnswers[index] = answer
+            setSelectedAnswers(updatedAnswers)
+        }
+    }, [answer.selectedAnswer])
 
     const getOptions = options.map(option => {
-        return(
+        return (
             <div className="form-check" key={nanoid()}>
-                <input className="form-check-input" type="radio" id={option} />
+                <input
+                    className="form-check-input"
+                    type="radio"
+                    id={option}
+                    name={question.id}
+                    value={option}
+                    checked={answer.selectedAnswer === option}
+                    onChange={e => {
+                        const newAnswer = { ...answer, selectedAnswer: e.target.value }
+                        setAnswer(newAnswer)
+                    }}
+                />
                 <label className="form-check-label" htmlFor={option}>{option}</label>
             </div>
         )
     })
 
     return (
-        <div className="card border-dark mt-2 ">
+        <div className="card border-dark mt-2" id={nanoid()}>
             <div className="card-header category">
                 {question.category}
             </div>
--- src/components/questions/Questions.js
+++ src/components/questions/Questions.js
@@ -4,6 +4,8 @@ import { getQuestions } from "@trivia-api/fetch";
 
 function Questions() {
     const [questions, setQuestions] = React.useState([])
+    const [selectedAnswers,setSelectedAnswers] = React.useState([])
+    const [score,setScore] = React.useState(null);
 
     React.useEffect(() => {
         getQuestions({ limit: 5 }).then((questions) => {
@@ -12,12 +14,26 @@ function Questions() {
     }, [])
 
     const mappedQuestions = questions.map(quest => {
-        return <Question key={quest.id} question={quest} />
+        return <Question key={quest.id} question={quest} selectedAnswers={selectedAnswers} setSelectedAnswers={setSelectedAnswers} />
     })
+
+    function checkAnswers(){
+        console.log(questions)
+        setScore(0)
+        for(let i=0; i<questions.length;i++){
+            if(selectedAnswers[i].selectedAnswer !== null && selectedAnswers[i].id === questions[i].id){
+                if(selectedAnswers[i].selectedAnswer === questions[i].correctAnswer){
+                    setScore(prev => prev+1)
+                }
+            }
+        }
+    }
 
     return (
         <div className="question justify-content-center">
             {mappedQuestions}
+            <button className="btn btn-primary" onClick={checkAnswers}>Click</button>
+            {score !== null && <span>Score : {score}</span>}
         </div>
     )
 }
