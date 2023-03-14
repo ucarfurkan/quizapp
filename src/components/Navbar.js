67bbdb69577c326c1a2432d83ed250a48694b9e0
ucarfurkan <furkanucar011@gmail.com>
2023-03-14 09:23:14 +0300

    components created, navbar set

---
+++ src/components/Navbar.js
@@ -0,0 +1,12 @@
+function Navbar() {
+    return (
+        <div className="navbar">
+            <div className="container">
+                <a className="navbar-brand navbar-name" href="#">Quiz App</a>
+                <span className="navbar-desc">... an app that you can test your knowledge!</span>
+            </div>
+        </div>
+    )
+}
+
+export default Navbar;
